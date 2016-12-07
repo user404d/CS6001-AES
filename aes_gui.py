2b39062e536d6202ce658bb4fe17c27bf39e2a9b
user404d <conduffql@gmail.com>
2016-12-06 20:05:35 -0600

    Implemented cbc and ecb modes

--- aes_gui.py
+++ aes_gui.py
@@ -14,19 +14,30 @@ def encrypt_click():
     file_path = path_entry.get();
     if  not os.path.isfile(file_path):
         log_text['state'] = 'normal'
-        log_text.insert('end', "Encrypt: File Not Found.")
+        log_text.insert('end', "Encrypt: Input File Not Found.\n")
         log_text['state'] = 'disabled'
+        
+    out_path = od_entry.get()
+    out_path = None if out_path == "" else out_path
 
     # getting key value from key entry field
     temp = key_entry.get();
     # splitting temp into list, delimitting every 2 characters
-    key = [int(temp[i:i+2]) for i in range(0, len(temp), 2)]
+    key = helper.convert_hex_to_bytes(temp)
     # generating key schedule from converted key
     key_schedule, _ = aes.gen_key_schedule(key)
 
-    aes_io.encrypt_file(key_schedule, file_path)
-    log_text['state'] = 'normal'
+    if enc_mode.get() == 0:
+        aes_io.encrypt_file(key_schedule, file_path, out_path)
+    else:
+        temp = iv_entry_text.get()
+        iv = helper.convert_hex_to_bytes(temp)
+        iv = helper.convert_to_state_matrix(iv)
+        aes_io.encrypt_file(key_schedule, file_path, out_path, 16, aes.Mode.cbc,
+                            iv)
 
+    log_text['state'] = 'normal'
+    log_text.insert('end', "Encrypt\n")
     log_text['state'] = 'disabled'
 
 """
@@ -36,39 +47,83 @@ def decrypt_click():
 
     # getting file and path and checking validity
     file_path = path_entry.get();
-    if  not os.path.isfile(file_path):
+    if not os.path.isfile(file_path):
         log_text['state'] = 'normal'
-        log_text.insert('end', "Encrypt: File Not Found.")
+        log_text.insert('end', "Decrypt: Encrypted File Not Found.\n")
         log_text['state'] = 'disabled'
+        return
+
+    out_path = od_entry.get()
+    out_path = None if out_path == "" else out_path
 
     # getting key value from key entry field
     temp = key_entry.get();
     # splitting temp into list, delimitting every 2 characters
-    key = [int(temp[i:i+2]) for i in range(0, len(temp), 2)]
+    key = helper.convert_hex_to_bytes(temp)
     # generating key schedule from converted key
     key_schedule, _ = aes.gen_key_schedule(key)
 
-    aes_io.decrypt_file(key_schedule, file_path, od_entry.get())
+    if enc_mode.get() == 0:
+        aes_io.decrypt_file(key_schedule, file_path, out_path)
+    else:
+        temp = iv_entry_text.get()
+        iv = helper.convert_hex_to_bytes(temp)
+        iv = helper.convert_to_state_matrix(iv)
+        aes_io.decrypt_file(key_schedule, file_path, out_path, 16, aes.Mode.cbc,
+                            iv)
 
     log_text['state'] = 'normal'
-    log_text.insert('end', "Decrypt")
-    log_text.insert('end', "\n")
+    log_text.insert('end', "Decrypt\n")
     log_text['state'] = 'disabled'
 
 """
 On click functionality of 'Gen. Key' button.
 """
-def genkey_click():
-    key = helper.generate_random_key(16)
+def gen_key_click():
+    key_size = key_size_bytes.get()
+    key = helper.generate_random_key(key_size)
+    
+    if len(key) != key_size:
+        print(key, key_size)
+        log_text['state'] = 'normal'
+        log_text.insert('end', "Error in Key Generation\n")
+        log_text['state'] = 'disabled'
+        return
 
-    temp = ""
-    for i in key:
-        temp += str(i)
-    key_entry_text.set(temp)
+    key_entry_text.set(helper.convert_bytes_to_hex(key))
+
+    print(key_entry_text.get())
 
     log_text['state'] = 'normal'
-    log_text.insert('end', "Generate Key")
-    log_text.insert('end', "\n")
+    log_text.insert('end', "Generate Key\n")
+    log_text['state'] = 'disabled'
+
+def gen_iv_click():
+    key_size = key_size_bytes.get()
+    iv = helper.initialization_vector(key_size)
+    
+    iv_entry_text.set(helper.convert_bytes_to_hex(iv))
+
+    log_text['state'] = 'normal'
+    log_text.insert('end', "Generate Initialization Vector.\n")
+    log_text['state'] = 'disabled'
+
+def ecb_mode_select():
+
+    gen_iv_button.config(state='disabled')
+    iv_entry.config(state='disabled')
+    
+    log_text['state'] = 'normal'
+    log_text.insert('end', "ECB mode selected\n")
+    log_text['state'] = 'disabled'
+
+def cbc_mode_select():
+
+    gen_iv_button.config(state='normal')
+    iv_entry.config(state='normal')
+    
+    log_text['state'] = 'normal'
+    log_text.insert('end', "CBC mode selected\n")
     log_text['state'] = 'disabled'
 
 
@@ -82,7 +137,10 @@ root.iconbitmap('aes.ico')
 
 path_entry_text = StringVar()
 key_entry_text = StringVar()
+iv_entry_text = StringVar()
 out_dir = StringVar()
+key_size_bytes = IntVar()
+enc_mode = IntVar()
 
 # Adding frame to root Tk window
 mainframe = ttk.Frame(root, padding="5")
@@ -97,24 +155,29 @@ fpkframe = ttk.Frame(mainframe)
 fpkframe.grid(column=0, row=0, sticky=W)
 
 # Label and text field entry for file path
-ttk.Label(fpkframe, text="File Path: ").grid(column=0, row=0, sticky=W)
+ttk.Label(fpkframe, text="File Path: ").grid(column=0, row=0, sticky=E)
 path_entry = ttk.Entry(fpkframe, width=40, textvariable=path_entry_text)
 path_entry.grid(column=1, row=0, sticky=(W,E)) # Stretches entry text field
 
-# Label and text field entry for key
-ttk.Label(fpkframe, text="Key: ").grid(column=0, row=1, sticky=E)
-key_entry = ttk.Entry(fpkframe, width=40, textvariable=key_entry_text)
-key_entry.grid(column=1, row=1, sticky=(E), pady=(10,0))
+# Label for output directory and textfield entry
+ttk.Label(fpkframe, text="Out Directoy: ").grid(column=0, row=1, sticky=W)
+od_entry = ttk.Entry(fpkframe, width=40, textvariable=out_dir)
+od_entry.grid(column=1, row=1, sticky=(E,W), pady=(10,0))
 
 # Seperator line between fpk and od frames
 ttk.Separator(mainframe, orient="horizontal").grid(column=0, row=1, sticky=(E,W))
 
-# Frame for output directory textfield entry
-odframe = ttk.Frame(mainframe)
-odframe.grid(column=0, row=2, sticky=(E,W))
-ttk.Label(odframe, text="Out Directoy: ").grid(column=0, row=0, sticky=W)
-od_entry = ttk.Entry(odframe, width=40, textvariable=out_dir)
-od_entry.grid(column=0, row=1, sticky=(E,W), padx=(65,0), pady=(5,0))
+#frame for encryption params
+encframe = ttk.Frame(mainframe)
+encframe.grid(column=0, row=2, sticky=(E,W))
+
+ttk.Label(encframe, text="Key: ").grid(column=0, row=1, sticky=E)
+key_entry = ttk.Entry(encframe, width=40, textvariable=key_entry_text)
+key_entry.grid(column=1, row=1, sticky=(E), padx=(11,0), pady=(10,0))
+
+ttk.Label(encframe, text="Initialization\n       Vector: ").grid(column=0, row=2, sticky=E)
+iv_entry = ttk.Entry(encframe, width=40, state='disabled', textvariable=iv_entry_text)
+iv_entry.grid(column=1, row=2, sticky=(E), padx=(11,0), pady=(10,0))
 
 # Seperator line between od and buttons frames
 ttk.Separator(mainframe, orient="horizontal").grid(column=0, row=3, sticky=(E,W))
@@ -124,16 +187,38 @@ buttonsframe = ttk.Frame(mainframe)
 buttonsframe.grid(column=0, row=4, sticky=(E, W))
 ttk.Button(buttonsframe, text="Encrypt", command=encrypt_click).grid(column=0, row=0, sticky=(E,W), padx=(20,20))
 ttk.Button(buttonsframe, text="Decrypt", command=decrypt_click).grid(column=1, row=0, sticky=(E,W), padx=(0,20))
-ttk.Button(buttonsframe, text="Gen. Key", command=genkey_click).grid(column=2, row=0, sticky=(E,W))
+ttk.Button(buttonsframe, text="Gen. Key", command=gen_key_click).grid(column=2, row=0, sticky=(E,W), padx=(0,20))
+gen_iv_button = ttk.Button(buttonsframe, text="Gen. IV", state='disabled', command=gen_iv_click)
+gen_iv_button.grid(column=3, row=0, sticky=(E,W))
+
+#Frame for holding logging and enc parameter selections
+bottomframe = ttk.Frame(mainframe)
+bottomframe.grid(column=0, row=5, sticky=(E,W))
 
 # Frame for holding log text container
-logframe = ttk.Frame(mainframe)
-logframe.grid(column=0, row=5, sticky=(E,W))
+logframe = ttk.Frame(bottomframe)
+logframe.grid(column=0, row=0)
 log_text = Text(logframe, state="disabled", width=40, height=10, wrap='word')
-log_text.grid(column=0, row=0, stick=(E,W))
+log_text.grid(column=0, row=0)
+
+modesframe = ttk.Frame(bottomframe)
+modesframe.grid(column=1, row=0, padx=(35,0))
+rbs = [ ttk.Radiobutton(modesframe, text='ECB', variable=enc_mode, value=0, command=ecb_mode_select),
+        ttk.Radiobutton(modesframe, text='CBC', variable=enc_mode, value=1, command=cbc_mode_select), ]
+for i,rb in enumerate(rbs): rb.grid(column=0, row=i)
+
+keysizeframe = ttk.Frame(bottomframe)
+keysizeframe.grid(column=2, row=0)
+keysizes = [ ttk.Radiobutton(keysizeframe, text='128', variable=key_size_bytes, value=16),
+             ttk.Radiobutton(keysizeframe, text='192', variable=key_size_bytes, value=24),
+             ttk.Radiobutton(keysizeframe, text='256', variable=key_size_bytes, value=32), ]
+for i,keysize in enumerate(keysizes): keysize.grid(column=0, row=i)
 
 # Adds padding around each frame container within mainframe
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+keysizes[0].invoke()
+rbs[0].invoke()
 
 # Main event loop needed to make everything run
 root.mainloop()
